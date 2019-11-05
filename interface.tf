7a9bcf6c51f70e369eede477f4e6e448575c08ec
rneighborsphone <rneighbors@yahoo.com>
2019-11-05 05:56:36 +0000

    First commit of vpc_basic module

---
+++ interface.tf
@@ -0,0 +1,43 @@
+variable "name" {
+    type = string
+    description = "The name of the VPC."
+}
+
+variable "cidr" {
+    type = string
+    description = "The CIDIR of the VPC."
+}
+
+
+variable "public_subnet" {
+    type = string
+    description = "The public subnet to create."
+}
+
+
+variable "enable_dns_hostnames" {
+    type = bool
+    description = "Should be true if you want to use private DNS within the VPC."
+    default = true
+}
+
+
+output "public_subnet_id" {
+    value = aws_subnet.public.id
+}
+
+
+output "vpc_id" {
+    value = aws_vpc.tfb.id
+}
+
+output "cidr" {
+    value = aws_vpc.tfb.cidr_block
+}
+
+output "public_subnet_id" {
+    value = aws_subnet.public.id
+    sensitive = true
+}
+
+
